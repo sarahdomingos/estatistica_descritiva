0a6b85755dc4dfb7b7e3b1cb6cdf226c99e996fb
SarahDomingos <92494941+sarahdomingos@users.noreply.github.com>
2022-09-16 14:00:59 -0300

    Update graficos_enem.R

--- graficos_enem.R
+++ graficos_enem.R
@@ -1,6 +1,6 @@
 
 #Coletar dados
-url <- "C:path do arquivo/Probabilidade e estatística/ENEM_AL_R_2022ok.csv"
+url <- "C:path do arquivo/ENEM_AL_R_2022ok.csv"
 milsa <- read.csv(url, header = TRUE, sep = ";", dec = ",")
 
 #a) Gerar um gráfico boxplot com todas as notas das provas, inclusive com a
@@ -16,23 +16,23 @@ boxplot(notas)
 limites <- c(0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000)
 classes <- c("0 - 100", "100 - 200", "200 - 300", "300 - 400", "400 - 500", "500 - 600", "600 - 700", "700 - 800", "800 - 900", "900 - 1000")
 
-freq_MA = table(cut(milsa$NU_NOTA_MT, breaks=limites, right=FALSE, labels=classes))
+freq_MA <- table(cut(milsa$NU_NOTA_MT, breaks=limites, right=FALSE, labels=classes))
 freq_ac_MA <- cumsum(freq_MA)
 freq_rel_MA <- prop.table(freq_MA)
 freq_rel_ac_MA <- cumsum(freq_rel_MA)
 
-freq_REDACAO = table(cut(milsa$NU_NOTA_REDACAO, breaks=limites, right=FALSE, labels=classes))
+freq_REDACAO <- table(cut(milsa$NU_NOTA_REDACAO, breaks=limites, right=FALSE, labels=classes))
 freq_ac_REDACAO <- cumsum(freq_REDACAO)
 freq_rel_REDACAO <- prop.table(freq_REDACAO)
 freq_rel_ac_REDACAO <- cumsum(freq_rel_REDACAO)
 
-tab_MA = cbind(freq_MA, freq_ac_MA, freq_rel_MA=round (freq_rel_MA*100, digits=2), freq_rel_ac_MA=round (freq_rel_ac_MA*100, digits=2))
+tab_MA <- cbind(freq_MA, freq_ac_MA, freq_rel_MA=round (freq_rel_MA*100, digits=2), freq_rel_ac_MA=round (freq_rel_ac_MA*100, digits=2))
 print(tab_MA)
-hist_MA = hist(tab_MA)
+hist_MA <- hist(tab_MA)
 
-tab_RE = cbind(freq_REDACAO, freq_ac_REDACAO, freq_rel_REDACAO=round (freq_rel_REDACAO*100, digits=2), freq_rel_ac_REDACAO=round (freq_rel_ac_REDACAO*100, digits=2))
+tab_RE <- cbind(freq_REDACAO, freq_ac_REDACAO, freq_rel_REDACAO=round (freq_rel_REDACAO*100, digits=2), freq_rel_ac_REDACAO=round (freq_rel_ac_REDACAO*100, digits=2))
 print(tab_RE)
-hist_RE = hist(tab_RE)
+hist_RE <- hist(tab_RE)
 
 #c) Com a média final (NU_NOTA_ENEM), agrupe os dados em 6 classes (0-300; 300-400, 400-500, 500-600, 700-1000) construir tabela com as
 #frequências absolutas e relativas e suas respectivas acumuladas. Gerar o
@@ -40,14 +40,14 @@ hist_RE = hist(tab_RE)
 limites <- c(0, 300, 400, 500, 600, 700, 1000)
 classes <- c("0 - 300", "300 - 400", "400 - 500", "500 - 600", "600 - 700", "700 - 1000")
 
-freq_FINAL = table(cut(milsa$NU_NOTA_ENEM, breaks=limites, right=FALSE, labels=classes))
+freq_FINAL <- table(cut(milsa$NU_NOTA_ENEM, breaks=limites, right=FALSE, labels=classes))
 freq_ac_FINAL <- cumsum(freq_FINAL)
 freq_rel_FINAL <- prop.table(freq_FINAL)
 freq_rel_ac_FINAL <- cumsum(freq_rel_FINAL)
 
-tab_FINAL = cbind(freq_FINAL, freq_ac_FINAL, freq_rel_FINAL=round (freq_rel_FINAL*100, digits=2), freq_rel_ac_FINAL=round (freq_rel_ac_FINAL*100, digits=2))
+tab_FINAL <- cbind(freq_FINAL, freq_ac_FINAL, freq_rel_FINAL=round (freq_rel_FINAL*100, digits=2), freq_rel_ac_FINAL=round (freq_rel_ac_FINAL*100, digits=2))
 print(tab_FINAL)
-hist_MA = hist(tab_FINAL)
+hist_MA <- hist(tab_FINAL)
 
 #d) Escolher duas variáveis (colunas) qualitativas, gerar gráficos adequados e
 #interpretar os resultados.
@@ -82,8 +82,4 @@ ggplot(data=df, aes(x=" ", y=value, group=internet, colour=internet, fill=intern
   geom_bar(width = 1, stat = "identity") +
   coord_polar("y", start=0) + 
   facet_grid(.~ subject) +theme_void() + ggtitle("Notas ENEM") + theme(plot.title = element_text(hjust=0.5, face='bold'))
-
-#f) Escolher duas variáveis (colunas) quantitativas, calcular o coeficiente de
-#correlação, gerar gráfico de dispersão, agrupar usando os quartis e
-#interpretar os resultados.
 
